0f99f70c20038355970b696a0c68a10b526c9487
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-06-22 16:59:27 +0800

    西元2024年06月22日 (週六) 16時59分27秒 CST

--- service-worker.js
+++ service-worker.js
@@ -16,7 +16,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v20240622-085433';
+const PRECACHE = 'precache-v20240622-085854';
 const RUNTIME = 'runtime';
 
 /**
